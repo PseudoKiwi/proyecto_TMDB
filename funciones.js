cbd0f6b678d97e2c3a203bd78d3949225bf52391
Pseudokiwi <tincho.perciante@gmail.com>
2023-11-01 15:04:59 -0300

    arreglos

--- funciones.js
+++ funciones.js
@@ -6,9 +6,11 @@ const https = require('https');
 
 
 // Función utilizada para obtener el API token del usuario
-function obtenerToken(email) {
+function obtenerToken(params) {
     return new Promise((resolve, reject) => {
         
+        const { email, password } = params;
+
         let token = ''; // token inicialmente vacío
         let encontrado = false; // variable para rastrear si se encontró el usuario
 
@@ -28,7 +30,13 @@ function obtenerToken(email) {
             if (user.email === email) {
                 token = user.token; // Defino el token como el token del usuario
                 encontrado = true;
-                lector.close(); // Detener la lectura una vez que se encuentra el usuario
+                const hashHex = crypto.createHash('sha256').update(password).digest('hex');
+                // Comparo tambien las contraseñas para saber si es correcto
+                if (user.password === hashHex) {
+                    lector.close(); // Detener la lectura una vez que se encuentra el usuario
+                } else {
+                    reject(new Error('Contraseña incorrecta'))
+                }
             }
         });
 
@@ -37,7 +45,7 @@ function obtenerToken(email) {
             if (encontrado) {
                 resolve(token); // Si se encontró el usuario, resuelvo la promesa con el token
             } else {
-                reject(new Error('El usuario no existe')); // Si no se encontró, rechazo la promesa lanzando un error
+                reject(new Error('El usuario no esta registrado')); // Si no se encontró, rechazo la promesa lanzando un error
             }
         });
 
@@ -49,9 +57,11 @@ function obtenerToken(email) {
 }
 
 // Función para obtener las películas favoritas del usuario seleccionado
-function favoritosUsuario(email) {
+function favoritosUsuario(params) {
     return new Promise((resolve, reject) => {
         
+        const { email } = params;
+
         let favoritas = []; // Lista de películas favoritas del usuario
         
         // Defino el lector para leer el archivo linea por linea
@@ -85,8 +95,10 @@ function favoritosUsuario(email) {
 }
 
 // Función utilizada para validar la autenticación del usuario
-function validacionToken(email, token) {
+function validacionToken(params) {
     return new Promise((resolve, reject) => {
+
+        const { email, token } = params;
 
         let validado = false; // Inicialmente, no se ha autenticado
 
@@ -122,8 +134,10 @@ function validacionToken(email, token) {
 }
 
 // Función para buscar las películas por keyword en TMDB
-function tmdbSearch(apiKey, keyword) {
+function tmdbSearch(params) {
     return new Promise((resolve, reject) => {
+
+        const { apiKey, keyword } = params;
 
         // Codifico la keyword de busqueda a formato de url
         const encodedKeyword = encodeURIComponent(keyword);
@@ -157,8 +171,10 @@ function tmdbSearch(apiKey, keyword) {
   }
 
 // Función para buscar las películas de la sección discover en TMDB
-function tmdbDiscover(apiKey) {
+function tmdbDiscover(params) {
     return new Promise((resolve, reject) => {
+
+        const { apiKey } = params;
 
         // Realizo la consulta https discover a la API de TMDB
         https.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`, (tmdbRes) => {
@@ -189,11 +205,13 @@ function tmdbDiscover(apiKey) {
 }
 
 // Función para buscar una película por id en TMDB
-function tmdbFind(apiKey, movieId) {
+function tmdbFind(params) {
     return new Promise((resolve, reject) => {
         
+        const { apiKey, id } = params;
+
         // Realizo la consulta https find a la API de TMDB con el id de la película buscada
-        https.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`, (tmdbRes) => {
+        https.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`, (tmdbRes) => {
             
             // Si la consulta no es exitosa, Rechazo la promesa
             if (tmdbRes.statusCode !== 200) {
@@ -221,9 +239,11 @@ function tmdbFind(apiKey, movieId) {
 }
 
 // Función para registrar un nuevo usuario
-function registrar(email, firstName, lastName, password) {
+function registrar(params) {
     return new Promise((resolve, reject) => {
         
+        const { email, firstName, lastName, password } = params;
+
         // Hasheo de contraseña con SHA256 y creación de token de autenticación de usuario
         const hashHex = crypto.createHash('sha256').update(password).digest('hex');
         const token = crypto.randomBytes(32).toString('hex');
@@ -270,9 +290,8 @@ function registrar(email, firstName, lastName, password) {
                 // Agrego el nuevo usuario al archivo users.txt
                 fs.appendFile('./users.txt', `${JSON.stringify(nuevo)}\n`, { encoding: 'utf8', flag: 'a' }, (error) => {
                     if (error) throw error;
-                    console.log('Nueva línea añadida correctamente');
+                    resolve('Usuario creado')
                 });
-                resolve('Usuario creado')
             } else {
                 reject(new Error("Usuario ya registrado"))
             }
@@ -286,133 +305,161 @@ function registrar(email, firstName, lastName, password) {
 }
 
 // Función utilizada para obtener las películas favoritas de un usuario autenticado
-async function obtenerFavoritos(email, token) {
+function obtenerFavoritos(params) {
+    return new Promise((resolve, reject) => {
+        const { email, token } = params;
 
-    // Valido que el token corresponda al usuario
-    const token_valido = await validacionToken(email, token);
-    
-    // De corresponderse, devuelvo las películas favoritas del usuario
-    // De lo contrario, retorno error
-    if (token_valido) {
+        // Valido que el token corresponda al usuario
+        validacionToken({email: email, token: token}).then( validado => {
+            
+            // De corresponderse, devuelvo las películas favoritas del usuario
+            // De lo contrario, retorno error
+            if (validado) {
 
-        // En favoritas guardo la lista de películas favoritas del usuario
-        var favoritas = await favoritosUsuario(email);
-        
-        // Modifico los elementos de favoritas agregando el campo suggestionForTodayScore a cada elemento
-        // suggestionForTodayScore es un número real aleatorio entre 0 y 99 
-        // Finalmente, ordeno la lista por suggestionForTodayScore de forma decreciente y retorno
-        return favoritas.map( fav => {
-            return { ...fav, suggestionForTodayScore: Math.random()*99 };
+                // En favoritas guardo la lista de películas favoritas del usuario
+                favoritosUsuario({email: email}).then(favoritas => {
+
+                    // Modifico los elementos de favoritas agregando el campo suggestionForTodayScore a cada elemento
+                    // suggestionForTodayScore es un número real aleatorio entre 0 y 99 
+                    // Finalmente, ordeno la lista por suggestionForTodayScore de forma decreciente y retorno
+                    resolve(favoritas.map( fav => {
+                        return { ...fav, suggestionForTodayScore: Math.random()*99 };
+                    })
+                    .sort((a, b) => {
+                        return b.suggestionForTodayScore - a.suggestionForTodayScore;
+                    }));
+                });
+            } else {
+                // Error de token incorrecto
+                reject(new Error("Validación incorrecta"));
+            }
         })
-        .sort((a, b) => {
-            return b.suggestionForTodayScore - a.suggestionForTodayScore;
+        .catch(error => {
+            reject(error)
         });
-        
-    } else {
-        // Error de token incorrecto
-        return new Error("Validación incorrecta")
-    }
+    });
 }
 
 // Función que busca las películas buscadas por keyword por un usuario autenticado
-async function obtenerBusqueda(email, token, apiKey, keyword) {
+async function obtenerBusqueda(params) {
 
-    // Valido que el token corresponda al usuario
-    const token_valido = await validacionToken(email, token);
+    return new Promise((resolve, reject) => {
+        const { email, token, apiKey, keyword } = params;
 
-    // De corresponderse, devuelvo las películas favoritas del usuario
-    // De lo contrario, retorno error
-    if (token_valido) {  
-
-        // Si el keyword es vacio, busco los discover. Sino, busco por keyword
-        if (keyword === '') {
+        // Valido que el token corresponda al usuario
+        validacionToken({email: email, token: token}).then( validado => {
             
-            // Realizo la solicitud de los discover
-            var resultado = await tmdbDiscover(apiKey);
+            // De corresponderse, devuelvo las películas favoritas del usuario
+            // De lo contrario, retorno error
+            if (validado) {
 
-            // Modifico los elementos de resultado agregando el campo suggestionScore a cada elemento
-            // suggestionScore es un número real aleatorio entre 0 y 99 
-            // Finalmente, ordeno la lista por suggestionScore de forma decreciente y retorno
-            return resultado.map(pelicula => {
-                return { ...pelicula, suggestionScore: Math.random()*99 };
-            }).sort((a, b) => {
-                return b.suggestionScore - a.suggestionScore;
-            });
+                // Si el keyword es vacio, busco los discover. Sino, busco por keyword
+                if (keyword === '') {
+                    
+                    // Realizo la solicitud de los discover
+                    tmdbDiscover({apiKey: apiKey}).then(resultado => {
 
-        } else {
-            
-            // Realizo la solicitud de la búsqueda por keyword
-            var resultado = await tmdbSearch(apiKey, keyword);
+                        // Modifico los elementos de resultado agregando el campo suggestionScore a cada elemento
+                        // suggestionScore es un número real aleatorio entre 0 y 99 
+                        // Finalmente, ordeno la lista por suggestionScore de forma decreciente y retorno
+                        resolve(resultado.map(pelicula => {
+                            return { ...pelicula, suggestionScore: Math.random()*99 };
+                        }).sort((a, b) => {
+                            return b.suggestionScore - a.suggestionScore;
+                        }));
+                    });
 
-            // Modifico los elementos de resultado agregando el campo suggestionScore a cada elemento
-            // suggestionScore es un número real aleatorio entre 0 y 99 
-            // Finalmente, ordeno la lista por suggestionScore de forma decreciente y retorno
-            return resultado.map(pelicula => {
-                return { ...pelicula, suggestionScore: Math.random()*99 };
-            }).sort((a, b) => {
-                return b.suggestionScore - a.suggestionScore;
-            });
-        };
-    } else {
-        // Error de token incorrecto
-        return new Error("Validación incorrecta")
-    };
+                } else {
+                    
+                    // Realizo la solicitud de la búsqueda por keyword
+                    tmdbSearch({apiKey: apiKey, keyword: keyword}).then(resultado => {
+
+                        // Modifico los elementos de resultado agregando el campo suggestionScore a cada elemento
+                        // suggestionScore es un número real aleatorio entre 0 y 99 
+                        // Finalmente, ordeno la lista por suggestionScore de forma decreciente y retorno
+                        resolve(resultado.map(pelicula => {
+                            return { ...pelicula, suggestionScore: Math.random()*99 };
+                        }).sort((a, b) => {
+                            return b.suggestionScore - a.suggestionScore;
+                        }));
+                    });
+                };
+                
+            } else {
+                // Error de token incorrecto
+                reject(new Error("Validación incorrecta"));
+            }
+        })
+        .catch(error => {
+            reject(error)
+        });
+    });
 }
 
 // Función que agrega a favoritos una película seleccionada por un usuario autenticado
-async function agregarFavorito(email, token, apiKey, movieId) {
+async function agregarFavorito(params) {
+    return new Promise((resolve, reject) => {
 
-    // Valido que el token corresponda al usuario
-    const token_valido = await validacionToken(email, token);
+        const {email, token, apiKey, id} = params
 
-    // De corresponderse, devuelvo las películas favoritas del usuario
-    // De lo contrario, retorno error
-    if (token_valido) {  
+        // Valido que el token corresponda al usuario
+        validacionToken({email: email, token: token}).then(validado => {
+            // De corresponderse, devuelvo las películas favoritas del usuario
+            // De lo contrario, retorno error
+            if (validado) {  
 
-        // Realizo la solicitud de la búsqueda por id
-        const resultado = await tmdbFind(apiKey, movieId);
-        resultado.email = email;        // Agrego el email del usuario
-        resultado.addedAt = new Date(); // Agrego el campo addedAt con la fecha actual
-        
-        // Defino el lector para leer el archivo linea por linea
-        // Utilizado para saber si el usuario ya existe en el sistema
-        const lector = readline.createInterface({
-            input: fs.createReadStream('./favoritos.txt'),
-            crlfDelay: Infinity
-        });
+                // Realizo la solicitud de la búsqueda por id
+                tmdbFind({apiKey: apiKey, id: id}).then(resultado => {
+                    resultado.email = email;        // Agrego el email del usuario
+                    resultado.addedAt = new Date(); // Agrego el campo addedAt con la fecha actual
+                    
+                    // Defino el lector para leer el archivo linea por linea
+                    // Utilizado para saber si el usuario ya existe en el sistema
+                    const lector = readline.createInterface({
+                        input: fs.createReadStream('./favoritos.txt'),
+                        crlfDelay: Infinity
+                    });
 
-        let aux = true; // Supongo que la película no está en favoritos
+                    let aux = true; // Supongo que la película no está en favoritos
 
-        // Función a ejecutar para cada línea del archivo
-        lector.on("line", (linea) => {
-            
-            // Transformo la linea de texto del archivo en un JSON
-            const favorito = JSON.parse(linea);
+                    // Función a ejecutar para cada línea del archivo
+                    lector.on("line", (linea) => {
+                        
+                        // Transformo la linea de texto del archivo en un JSON
+                        const favorito = JSON.parse(linea);
 
-            // Entro al condicional si la película ya está ingresada como favorita para ese usuario
-            if (resultado.id === favorito.id && resultado.email === favorito.email) {
-                aux = false;    // La película ya está en favoritos
-                lector.close(); // Cierro el lector porque ya está ingresada a favoritos de ese usuario
-            }
-        });
+                        // Entro al condicional si la película ya está ingresada como favorita para ese usuario
+                        if (resultado.id === favorito.id && resultado.email === favorito.email) {
+                            aux = false;    // La película ya está en favoritos
+                            lector.close(); // Cierro el lector porque ya está ingresada a favoritos de ese usuario
+                        }
+                    });
 
-        // Código a ejecutar cuando el lector se cierra
-        lector.on("close", () => {
+                    // Código a ejecutar cuando el lector se cierra
+                    lector.on("close", () => {
 
-            // Si la película no está en la lista de favoritas del usuario, entro al condicional
-            if (aux) {
-                
-                // Agrego al archivo favoritos.txt el JSON de la película seleccionada
-                fs.appendFile('./favoritos.txt', `${JSON.stringify(resultado)}\n`, { encoding: 'utf8', flag: 'a' }, (error) => {
-                    if (error) throw error;
-                    console.log('Nueva línea añadida correctamente');
+                        // Si la película no está en la lista de favoritas del usuario, entro al condicional
+                        if (aux) {
+                            
+                            // Agrego al archivo favoritos.txt el JSON de la película seleccionada
+                            fs.appendFile('./favoritos.txt', `${JSON.stringify(resultado)}\n`, { encoding: 'utf8', flag: 'a' }, (error) => {
+                                if (error) throw error;
+                                resolve("Favorito correctamente ingresado");
+                            });
+                        } else {
+                            reject(new Error("Favorito ya existente"))
+                        }
+                    });
                 });
-            }
+            } else {
+                // Error de token incorrecto
+                reject(new Error("Validación incorrecta"))
+            };
+        })
+        .catch(error => {
+            reject(error);
         });
-    } else {
-        // Error de token incorrecto
-        return new Error("Validación incorrecta")
-    };
+    });
 }
 
 // Declaración de las funciones públicas
--- main.js
+++ main.js
@@ -11,23 +11,21 @@ const app = express();
 // Permito que la aplicación reciba solicitudes en formato JSON
 app.use(express.json());
 
-// Inicializo el token de sesion. Dejará de ser nulo cuando un usuario se autentique
-var tokenDeSesion = '';
-
 // ------------------------ GET ENDPOINTS ------------------------ //
 
 // Solicitud GET para obtener favoritos del usuario autenticado
 app.get('/favoritos', (req, res) => {
 
   // Desarmo la solicitud con los datos necesarios del request
-  const email = req.body.email;
+  const { email } = req.body;
+  const token = req.headers.token;;
 
   // Llamo a la función de obtener favoritos y retorno al cliente la lista de JSON
-  funciones.obtenerFavoritos(email, tokenDeSesion).then((favoritos, error) => {
+  funciones.obtenerFavoritos({email: email, token: token}).then((favoritos, error) => {
     if (error) throw error;
-    res.status(200).json(favoritos);
+    res.status(200).json({resultado: favoritos});
   }).catch(error => {
-    res.status(403).json({ mensaje: error.message });
+    res.status(400).json({message: error.message});
   });
 
 });
@@ -37,13 +35,14 @@ app.get('/buscar', (req, res) => {
 
   // Desarmo la solicitud con los datos necesarios del request
   const { email, keyword } = req.body;
+  const token = req.headers.token;
 
   // Llamo a la función de busqueda y retorno al cliente la lista de JSON
-  funciones.obtenerBusqueda(email, tokenDeSesion, apiKey, keyword).then((result, error) => {
+  funciones.obtenerBusqueda({email: email, token: token, apiKey: apiKey, keyword: keyword}).then((resultado, error) => {
     if (error) throw error;
-    res.status(200).json(result);
+    res.status(200).json({resultado: resultado});
   }).catch(error => {
-    res.status(500).json({ mensaje: 'Error al procesar la solicitud' });
+    res.status(400).json({message: error.message});
   });
 
 });
@@ -59,12 +58,12 @@ app.post('/registrar', (req, res) => {
   const { email, firstName, lastName, password } = req.body;
 
   // Llamo a la función registrar con los datos necesarios
-  funciones.registrar(email, firstName, lastName, password).then((resultado, error) => {
+  funciones.registrar({email: email, firstName: firstName, lastName: lastName, password: password}).then((resultado, error) => {
     if (error) throw error;
-    res.status(200).json({ message: resultado});
+    res.status(200).json({message: resultado});
   })
   .catch( error => {
-    res.status(400).json({message:error.message});
+    res.status(400).json({message: error.message});
   });
 
 });
@@ -73,16 +72,14 @@ app.post('/registrar', (req, res) => {
 app.post('/autenticar', (req, res) => {
 
   // Desarmo la solicitud con los datos necesarios del request
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   // Llamo a la función autenticar con el email del usuario y retorno el token a la aplicación cliente
-  funciones.obtenerToken(email).then((resultado, error) => {
+  funciones.obtenerToken({email: email, password: password}).then((token, error) => {
     if (error) throw error;
-    tokenDeSesion = resultado;
-    console.log(tokenDeSesion)
-    res.status(200).json({ message: "Usuario autenticado" });
+    res.status(200).json({message: "Usuario autenticado", token: token});
   }).catch(error => {
-    res.status(400).json({message:error.message});
+    res.status(400).json({message: error.message});
   });
 
 });
@@ -92,13 +89,14 @@ app.post('/agregarFavorito', async (req, res) => {
   
   // Desarmo la solicitud con los datos necesarios del request
   const { email, id } = req.body;
+  const token = req.headers.token;;
   
   // Llamo a la función agregarFavorito para agregar la película especificada a los favoritos del usuario
-  funciones.agregarFavorito(email, tokenDeSesion, apiKey, id).then((error) => {
+  funciones.agregarFavorito({email: email, token: token, apiKey: apiKey, id: id}).then((resultado, error) => {
     if (error) throw error;
-    res.status(200).json({ message: "Favorito correctamente ingresado"});
+    res.status(200).json({message: resultado});
   }).catch(error => {
-    res.status(500).json({ mensaje:error.message });
+    res.status(400).json({message: error.message});
   });
 
 });
